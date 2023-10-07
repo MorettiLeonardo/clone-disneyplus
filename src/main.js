c91102b27a8dda81b03b16317accdba7ac0764bf
Leonardo Moretti <leonardonabarro2003@gmail.com>
2023-10-07 12:59:49 -0300

    criando as tabs

---
+++ src/main.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]')
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(button) {
+            const tabTarget = button.target.dataset.tabButton
+            const tab = document.querySelector(`[data-tab-id=${tabTarget}]`)
+            
+            hideAllTabs()
+            tab.classList.add('shows__list--is-active')
+            
+            hideAllButton()
+            button.target.classList.add('shows__tabs__button--is-active')
+        })
+    }
+})
+
+function hideAllButton() {
+    const buttons = document.querySelectorAll('[data-tab-button]')
+
+    for (let i = 0;i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active')
+    }
+}
+
+function hideAllTabs() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]')
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__list--is-active')
+    }
+}
